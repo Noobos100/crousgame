bc0130d29e3493453cdade0486b40814200dd8fe
Axel <115540473+ROUBAUD-Axel-2225073b@users.noreply.github.com>
2025-01-03 15:13:18 +0100

    fix: modification script Win

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -1,34 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Assurez-vous d'inclure ce namespace
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     // Variable pour vérifier si le niveau est gagné
     private bool levelWon = false;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // Initialisation si nécessaire
     }
 
     // Update is called une fois par frame
     void Update()
     {
-            // Faire tourner le trophée sur l'axe Y
-            transform.Rotate(Vector3.up * Time.deltaTime * 50);
-        
+        transform.Rotate(Vector3.up * Time.deltaTime * 50);
     }
 
     // Méthode pour appeler lorsque le niveau est gagné
     public void WinLevel()
     {
-        Debug.Log("Win!");
-        levelWon = true;
-        // Charger la scène de victoire
-        SceneManager.LoadScene("Win");
+        if (!levelWon)
+        {
+            Debug.Log("Win!");
+            levelWon = true;
+            // Charger la scène de victoire
+            SceneManager.LoadScene("Win");
+        }
     }
 
     // Détecter la collision avec le joueur
